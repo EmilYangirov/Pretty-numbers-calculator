aa8503cc6894928d4685fae30b3a12d0b598e226
Emil <Emilyangirov@gmail.com>
2022-12-09 23:19:17 +0500

    Release

---
+++ Pretty numbers calculator/Calculator/Calculator/PrettyCalculator.cs
@@ -0,0 +1,68 @@
+﻿
+namespace Calculator;
+public class PrettyCalculator
+{
+    public long PrettyNumbersCount(int numLength, int numBase)
+    {
+        if (!CorrectNumber(numLength, numBase))
+            return 0;
+
+        //находим максимальную возможную сумму чисел
+        int half = numLength / 2;
+        Console.WriteLine("half = " + half);
+
+        //находим количество цифр в половине
+        int maxSum = half * (numBase - 1);
+        Console.WriteLine("max sum = " + maxSum);
+
+        //вычисляем количество комбинаций чисел для каждой возможной суммы
+        long count = 0;
+        for (int i = 0; i <= maxSum; i++)
+        {
+            var k = CalculateSumCount(half, i, numBase);
+            Console.WriteLine("N(" + i + ") = " + k);
+            count += k*k;
+        }
+
+        //вычисление центрального числа
+        int koeff = (numLength % 2 == 0) ? 1 : numBase;
+
+        return koeff*count;
+
+    }
+
+    //Функция расчета количества комбинаций для суммы
+    private long CalculateSumCount(int length, int sum, int numBase)
+    {
+        if (length == 1)
+            return (sum < numBase && sum >= 0) ? 1 : 0;
+
+        long count = 0;
+
+        for(int i = 0; i < numBase; i++)
+        {
+            var k = CalculateSumCount(length - 1, sum - i, numBase);
+            count += k;
+        }
+
+        return count;
+    }
+
+    //Проверка числа на корректность
+    private bool CorrectNumber(int numLength, int numBase)
+    {
+        if (numLength < 2)
+        {
+            Console.WriteLine("Количество цифр должно быть больше или равно 2");
+            return false;
+        }
+
+        if(numBase < 2)
+        {
+            Console.WriteLine("Система счисления должна быть большей или равной 2");
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Pretty numbers calculator/Calculator/Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Calculator;
+
+PrettyCalculator calc = new PrettyCalculator();
+
+Console.WriteLine(calc.PrettyNumbersCount(13,13));
